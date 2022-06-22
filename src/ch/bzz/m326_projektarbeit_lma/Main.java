4989b3fd9508f5b856203ffe1cd59b9d6929930f
Alexander Benak <benaka@bzz.ch>
2022-06-22 10:45:17 +0200

    javadoc in Main

--- src/ch/bzz/m326_projektarbeit_lma/Main.java
+++ src/ch/bzz/m326_projektarbeit_lma/Main.java
@@ -11,6 +11,10 @@ import ch.bzz.m326_projektarbeit_lma.gui.MainFrame;
  */
 public class Main {
 
+    /**
+     * The main method that creates the GUI
+     * @param args
+     */
     public static void main(String[] args) {
         // automatically load the Data
         JSONData.getInstance();
